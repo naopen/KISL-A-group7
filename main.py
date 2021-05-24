747537a8cc8dae9e5d73b7dfcbfa6bd7c850c2bc
naopen <naokizssm@gmail.com>
2021-05-24 16:55:52 +0900

    flask追加

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #------------------------ def ------------------------ 
 
 #通常のQueryを作成するbasic_query関数を定義
+<<<<<<< Updated upstream
 def basic_query(budget):
     import sqlite3
     con = sqlite3.connect("test.db")
@@ -8,6 +9,78 @@ def basic_query(budget):
     query = "select * from test where item_price <= {};".format(budget)
     return query
     con.close()
+=======
+def is_int(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+        
+def make_query(budget,areas):
+    query = "select * from test "
+
+    area_list = []
+    a_num = 0
+    for area in areas:
+      if area == "1":
+        area_list.append('エリアA')
+        a_num += 1
+      else:
+        a_num = a_num 
+      if area == "2":
+        area_list.append('エリアB')
+        a_num += 1
+      else:
+        a_num = a_num
+      if area == "3":
+        area_list.append('エリアC')
+        a_num += 1
+      else:
+        a_num = a_num
+      if area == "4":
+        area_list.append('エリアD')
+        a_num += 1
+      else:
+        a_num = a_num
+      if area == "5":
+        area_list.append('エリアE')
+        a_num += 1
+      else:
+        a_num = a_num
+      
+    query_area = "" 
+    if a_num == 0:
+      query_area = ""
+    elif a_num == 1:
+      for area in area_list:
+        query_area = "where store_area = '{}' ".format(area)  
+    else:
+      count = 1
+      query_area = "where ("
+      for area in area_list:
+        if count == 1:
+          query_area += "store_area = '{}' ".format(area)
+          count +=1
+        elif count == a_num:
+          query_area += "or store_area = '{}' ) ".format(area)
+          count +=1
+        else:
+          query_area += "or store_area = '{}' ".format(area)
+          count +=1
+    
+    query += query_area
+
+    if  is_int(budget):
+      if a_num == 0:
+        query += "where item_price <= {} ;".format(budget)
+      else:
+        query += "and item_price <= {} ;".format(budget)
+    else:
+      query += ";"
+
+    return query
+>>>>>>> Stashed changes
 
 #------------------------ Flask ------------------------ 
 
@@ -28,8 +101,20 @@ def basic_search():
   con = sqlite3.connect("test.db")
   cur = con.cursor()
   page = 1
+<<<<<<< Updated upstream
   budget = request.form["budget"]
   query = basic_query(budget)
+=======
+
+  #予算
+  budget = request.form["budget"]
+
+  #エリア
+  areas = request.form.getlist('area') # ['1', '2', '4']
+
+  query = make_query(budget,areas)
+
+>>>>>>> Stashed changes
   displays_list = []
   displays_count = 0
   for row in cur.execute(query):
